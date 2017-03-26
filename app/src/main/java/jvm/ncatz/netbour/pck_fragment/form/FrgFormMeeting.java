7e239bb422daf34690927edcac51c434e29c962c
JMedinilla <javimedinilla@gmail.com>
2017-03-26 02:52:00 +0100

    Fragments implemented

--- app/src/main/java/jvm/ncatz/netbour/pck_fragment/form/FrgFormMeeting.java
+++ app/src/main/java/jvm/ncatz/netbour/pck_fragment/form/FrgFormMeeting.java
@@ -1,4 +1,58 @@
 package jvm.ncatz.netbour.pck_fragment.form;
 
-public class FrgFormMeeting {
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import jvm.ncatz.netbour.R;
+
+public class FrgFormMeeting extends Fragment {
+    private FormMeeting callback;
+
+    interface FormMeeting {
+
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_form_meeting, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        callback = (FormMeeting) context;
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        callback = null;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
 }
--- app/src/main/java/jvm/ncatz/netbour/pck_fragment/list/FrgDocument.java
+++ app/src/main/java/jvm/ncatz/netbour/pck_fragment/list/FrgDocument.java
@@ -1,4 +1,58 @@
 package jvm.ncatz.netbour.pck_fragment.list;
 
-public class FrgDocument {
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import jvm.ncatz.netbour.R;
+
+public class FrgDocument extends Fragment {
+    private ListDocument callback;
+
+    interface ListDocument {
+
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_list_document, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        callback = (ListDocument) context;
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        callback = null;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
 }
